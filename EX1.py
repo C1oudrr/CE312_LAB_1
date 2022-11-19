01e047e7d09b78901210cf8edbf9b92087a362a5
C1oudrr <98571454+C1oudrr@users.noreply.github.com>
2022-11-19 21:41:54 +0700

    Update EX1.py

--- EX1.py
+++ EX1.py
@@ -1,13 +1,7 @@
 row = int(input("Insert Number of rows : "))
-num = int(input("Insert Number of Diamond : "))
 
-def diamond(num,row):
-    for k in range(num):
+for i in range(row):
+    print(" "*(row-i)+" *"*(i+1))
 
-        for i in range(row):
-            print(" "*(row-i)+" *"*(i+1))
-
-        for j in range(row-1):
-            print(" "*(j+2)+" *"*(row-1-j))
-
-diamond(num,row)
+for j in range(row-1):
+    print(" "*(j+2)+" *"*(row-1-j))
