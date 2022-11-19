0bacee3e8bc00d87cf22e63a484f1f8bd29430e7
C1oudrr <98571454+C1oudrr@users.noreply.github.com>
2022-11-19 21:41:36 +0700

    Update EX2.py

--- EX2.py
+++ EX2.py
@@ -1,19 +1,13 @@
-print("Summation of harmonic series")
-def cal(n):
-        if n < 2:
-            return 1
+row = int(input("Insert Number of rows : "))
+num = int(input("Insert Number of Diamond : "))
 
-        else:
-            return 1 / n + (cal(n - 1))
+def diamond(num,row):
+    for k in range(num):
 
-print("Limit = 1 Value = ",cal(1))
-print("Limit = 2 Value = ",cal(2))
-print("Limit = 3 Value = ",cal(3))
-print("Limit = 4 Value = ",cal(4))
-print("Limit = 5 Value = ",cal(5))
-print("Limit = 6 Value = ",cal(6))
-print("Limit = 7 Value = ",cal(7))
-print("Limit = 8 Value = ",cal(8))
-print("Limit = 9 Value = ",cal(9))
-print("Limit = 10 Value = ",cal(10))
-print(cal(10))
+        for i in range(row):
+            print(" "*(row-i)+" *"*(i+1))
+
+        for j in range(row-1):
+            print(" "*(j+2)+" *"*(row-1-j))
+
+diamond(num,row)
